d5d287a0aaee37effedd0d228841989e1cc9cf96
Jake Goulding <jake.goulding@gmail.com>
2014-06-08 19:56:17 -0400

    Add Minitest example

---
+++ minitest/test/test_alpha.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+
+class TestAlpha < MiniTest::Unit::TestCase
+  def test_that_passes
+    assert_equal 1, 1
+  end
+
+  def test_that_fails
+    refute_equal 1, 1
+  end
+
+  def test_that_will_be_skipped
+    skip "test this later"
+  end
+end
